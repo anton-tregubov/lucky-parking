d297adab0d3047c8f50aefec0f227a5401158af7
Anton Tregubov <tregubov@gmail.com>
2021-08-26 04:05:28 +0300

    Refactoring

--- ci/run-local.sh
+++ ci/run-local.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")"
 
 export DOCKER_IMAGE_REPOSITORY=local
 export DOCKER_IMAGE_PUSH=false
+export DOCKER_IMAGE_PULL_POLICY=Never
 source ./run-common.sh
 export K8S_NAMESPACE=${APPLICATION_NAME}-local
 export DOMAIN=kubernetes.docker.internal
