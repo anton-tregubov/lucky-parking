54fc24b6b6f02520c8b86c1b64a40ee917c77a8d
Anton Tregubov <tregubov@gmail.com>
2021-09-13 19:41:08 +0300

    Configure Keycloak

--- src/main/java/ru/faulab/lucky/parking/ExampleResource.java
+++ src/main/java/ru/faulab/lucky/parking/ExampleResource.java
@@ -1,7 +1,10 @@
 package ru.faulab.lucky.parking;
 
+import io.quarkus.security.identity.SecurityIdentity;
 import io.smallrye.mutiny.Uni;
 
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
@@ -9,11 +12,31 @@ import javax.ws.rs.core.MediaType;
 public class ExampleResource
 {
 
+    @Inject
+    SecurityIdentity securityIdentity;
+
     @GET
     @Path("/hello")
     @Produces(MediaType.APPLICATION_JSON)
     public Uni<Dto> hello()
     {
         return Uni.createFrom().item(new Dto("String", 42));
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/no-role")
+    public String noRoles()
+    {
+        return "Access for " + securityIdentity.getPrincipal().getName() + " is granted. Roles are: " + securityIdentity.getRoles();
+    }
+
+    @GET
+    @RolesAllowed("lucky-parking-access")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/secured")
+    public String secured()
+    {
+        return "Access for " + securityIdentity.getPrincipal().getName() + " is granted";
     }
 }
